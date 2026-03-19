e1d9fe5f148d30e55a28cad41ae4e27a823c2dbc
Brian Davis <kbdavis07@yahoo.com>
2026-03-19 14:30:32 -0400

    (WiP) Add Aspire.Extensions project with Keycloak ATS support
    Added Aspire.Extensions project targeting .NET 10.0, referencing Aspire.Hosting and Aspire.Hosting.Keycloak. Introduced AspireExportAttribute for ATS exports. Implemented KeycloakResource and related builder extensions for Keycloak container configuration, including data volumes, realm imports, feature toggling, and OTLP exporter setup. Updated solution file to include the new project.

---
+++ src/Extensions/Aspire.Extensions/Keycloak/KeycloakResource.cs
@@ -0,0 +1,50 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Aspire.Custom.Hosting;
+using Aspire.Hosting;
+using Aspire.Hosting.ApplicationModel;
+
+namespace Aspire.Custom.ApplicationModel;
+
+/// <summary>
+/// A resource that represents a Keycloak resource.
+/// </summary>
+/// <param name="name">The name of the resource.</param>
+/// <param name="admin">A parameter that contains the Keycloak admin, or <see langword="null"/> to use a default value.</param>
+/// <param name="adminPassword">A parameter that contains the Keycloak admin password.</param>
+#pragma warning disable ASPIREATS001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+[AspireExport(ExposeProperties = true)]
+#pragma warning restore ASPIREATS001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+public sealed class KeycloakResource(string name, ParameterResource? admin, ParameterResource adminPassword)
+    : ContainerResource(name), IResourceWithServiceDiscovery
+{
+    private const string DefaultAdmin = "admin";
+    internal const string PrimaryEndpointName = "tcp";
+
+    /// <summary>
+    /// Features to enable for the keycloak resource
+    /// </summary>
+    internal HashSet<string> EnabledFeatures { get; } = new();
+
+    /// <summary>
+    /// Features to disable for the keycloak resource
+    /// </summary>
+    internal HashSet<string> DisabledFeatures { get; } = new();
+
+    /// <summary>
+    /// Gets the parameter that contains the Keycloak admin.
+    /// </summary>
+    public ParameterResource? AdminUserNameParameter { get; } = admin;
+
+    internal ReferenceExpression AdminReference =>
+        AdminUserNameParameter is not null ?
+            ReferenceExpression.Create($"{AdminUserNameParameter}") :
+            ReferenceExpression.Create($"{DefaultAdmin}");
+
+    /// <summary>
+    /// Gets the parameter that contains the Keycloak admin password.
+    /// </summary>
+    public ParameterResource AdminPasswordParameter { get; } = adminPassword ?? throw new ArgumentNullException(nameof(adminPassword));
+}
+

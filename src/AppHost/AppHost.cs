ea3f0f61dcc9548b8734464c7e1f007cc6ca596b
Brian Davis <kbdavis07@yahoo.com>
2026-03-19 17:25:45 -0400

    Replace Keycloak container with AddKeycloak resource
    Switched from manual Keycloak container setup to builder.AddKeycloak, enabling persistent data, realm import, and Postgres integration. Added custom UDP HTTPS endpoint and removed legacy container configuration, including admin credentials and certificate mounts.

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -1,3 +1,5 @@
+using System.Net.Sockets;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
@@ -11,44 +13,24 @@ var database = postgres.AddDatabase("keycloak-db");
 
 var keycloakDbUrl = ReferenceExpression.Create($"jdbc:postgresql://{postgres.Resource.Host}/{database.Resource.DatabaseName}");
 
-var keycloak2 = builder.AddKeycloak("keycloak2", 9999)
+var keycloak = builder.AddKeycloak("keycloak", 9999)
                       .WithLifetime(ContainerLifetime.Persistent)
                       .WithDataVolume()
                       .WithRealmImport("./Realms")
-                      //.WithReference(postgres)
-                      //.WaitFor(postgres)
-                      //.WithEnvironment("KC_DB", "postgres") // Database "Type" not Name
-                      //.WithEnvironment("KC_DB_USERNAME", postgres.Resource.UserNameReference)
-                      //.WithEnvironment("KC_DB_PASSWORD", postgres.Resource.PasswordParameter)
-                      //.WithEnvironment("KC_DB_URL", keycloakDbUrl)
+                      .WithReference(postgres)
+                      .WaitFor(postgres)
+                      .WithEnvironment("KC_DB", "postgres") // Database "Type" not Name
+                      .WithEnvironment("KC_DB_USERNAME", postgres.Resource.UserNameReference)
+                      .WithEnvironment("KC_DB_PASSWORD", postgres.Resource.PasswordParameter)
+                      .WithEnvironment("KC_DB_URL", keycloakDbUrl)
                       .WithEnvironment("KC_HTTP_ENABLED", "false")
+                      .WithEndpoint("keycloak-https", e => {
+                          e.Port = 9999;
+                          e.TargetPort = 8443;
+                          e.Protocol = ProtocolType.Udp;
+                          e.UriScheme = "https";
+                      })
                       .WithOtlpExporter();
-
-var keycloak = builder.AddContainer("keycloak", "quay.io/keycloak/keycloak:26.4")
-    .WithEnvironment("KC_DB", "postgres")
-    .WithEnvironment("KC_DB_USERNAME", postgres.Resource.UserNameReference)
-    .WithEnvironment("KC_DB_PASSWORD", postgres.Resource.PasswordParameter)
-    .WithEnvironment("KC_DB_URL", keycloakDbUrl)
-    .WithEnvironment("KC_HTTP_ENABLED", "false")
-    .WithEnvironment("KC_BOOTSTRAP_ADMIN_PASSWORD", "N}Hh)k_PgT7ArDh))1nT2B")
-    .WithEnvironment("KC_BOOTSTRAP_ADMIN_USERNAME", "admin")
-    .WithEnvironment("KC_HTTPS_CERTIFICATE_FILE", "/usr/lib/ssl/aspire/private/8BFD721862BF6F0296392F4F92F9AF4B2176A160.crt")
-    .WithEnvironment("KC_HTTPS_CERTIFICATE_KEY_FILE", "/usr/lib/ssl/aspire/private/8BFD721862BF6F0296392F4F92F9AF4B2176A160.key")
-    .WithBindMount("./Realms", "/opt/keycloak/data/import")
-    .WithArgs("start-dev", "--import-realm")
-    .WithEndpoint("https", e =>
-    {
-        e.Port = 9999;
-        e.TargetPort = 8443;
-        e.UriScheme = "https";
-    })
-    .WithEndpoint("admin", e =>
-    {
-        e.Port = 9000;
-        e.TargetPort = 9000;
-        e.UriScheme = "http";
-    })
-    .WithOtlpExporter();
 
 
 var server = builder.AddProject<Projects.GatewayHost>("gatewayhost")
